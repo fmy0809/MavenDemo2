67b99546051eac8f2f21e5088c832ffea4e7991c
fmy0809 <517786327@qq.com>
2018-03-13 23:26:17 +0800

    fff2

--- web/src/main/java/com/fmy/controller/test/TestController.java
+++ web/src/main/java/com/fmy/controller/test/TestController.java
@@ -1,8 +1,12 @@
 package com.fmy.controller.test;
 
+import com.fmy.dao.TestDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 /**
  * Created by Administrator on 2018/3/11.
@@ -10,9 +14,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
 
+    @Autowired
+    private TestDao testDao;
+
     @GetMapping("/test")
     @ResponseBody
     public String say(){
         return "Hello demo2!";
+    }
+
+    @GetMapping("/test/dao")
+    @ResponseBody
+    public Map<String,Object> getTest(){
+
+        return testDao.getTest();
     }
 }
